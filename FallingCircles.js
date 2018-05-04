d7fb22668dd6cfab1a9f4dc7806b0d94e929f925
Max Frischknecht <max@debutdebut.com>
2018-05-04 14:03:59 +0200

    no blendmode, tend to the right

--- FallingCircles.js
+++ FallingCircles.js
@@ -6,7 +6,7 @@ class FallingCircles {
     this.y = y;
     this.r = 10;
     this.gravity = random(-2, 2);
-    this.gravity2 = random(-2, 2);
+    this.gravity2 = random(2, 5);
 
     this.val = value;
     this.saturation = 100;
--- sketch.js
+++ sketch.js
@@ -5,7 +5,6 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	noStroke();
 	background(5);
-	blendMode(LIGHTEST);
 
 	xpos = width / 2;
   ypos = height / 2;
